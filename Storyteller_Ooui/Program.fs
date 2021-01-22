d9f93faeb9410f62d53db3d37c352993f5422dba
Jeff Kessleman <jeffpk@worldwizards.net>
2021-01-21 22:44:49 -0500

    Basic infrastructure working

---
+++ Storyteller_Ooui/Program.fs
@@ -0,0 +1,28 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+open System
+open Ooui
+
+// Define a function to construct a message to print
+
+
+[<EntryPoint>]
+let main argv =
+     let button = Button("Click me!");
+
+        // Add some logic to it
+     let mutable count = 0;
+     button.Click.Add(
+         fun evt ->
+            count <- count + 1
+            button.Text <- $"Clicked {count} times"
+            ()
+     )
+
+        // Publishing makes an object available at a given URL
+        // The user should be directed to http://localhost:8080/shared-button
+     UI.Port <- 5370   
+     UI.Publish ("/shared-button", button)
+
+        // Don't exit the app until someone hits return
+     Console.ReadLine()
+     0
